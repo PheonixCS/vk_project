357adbac310441292397c279763637de8ac5379b
Alexander Bogdanov <netolyrg@gmail.com>
2018-05-06 21:40:12 +0300

    celery works

--- vk_scraping_posting/celery.py
+++ vk_scraping_posting/celery.py
@@ -1,18 +1,19 @@
 # celery instance file
-from __future__ import absolute_import
+from __future__ import absolute_import, unicode_literals
 
 import os
 
 from celery import Celery
-from django.conf import settings
 
-os.environ.setdefault('DJANGO_SETTING_MODULE', 'vk_scraping_posting.settings')
+# TODO figure out why 
+os.environ['DJANGO_SETTINGS_MODULE'] = 'vk_scraping_posting.settings'
+# os.environ.setdefault('DJANGO_SETTING_MODULE', 'vk_scraping_posting.settings')
 
 app = Celery('vk_scraping_posting')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+app.autodiscover_tasks()
 
 app.task(bind=True)
 
