50fd3052f2f893a326ed381beaa07210f0bab63d
poff <alexander.odnoral@gmail.com>
2018-05-07 10:20:48 +0300

    add str, add filter fields verbose names

--- scraping/models.py
+++ scraping/models.py
@@ -5,14 +5,25 @@ class Donor(models.Model):
     id = models.CharField(max_length=32, verbose_name='Domain/id группы донора', primary_key=True)
     is_involved = models.BooleanField(default=True, verbose_name='Донор задействован?')
 
+    def __str__(self):
+        return self.id
+
 
 class Filter(models.Model):
     donor = models.ForeignKey(Donor, on_delete=models.DO_NOTHING)
-    min_quantity_of_line_breaks = models.IntegerField(blank=True, null=True)
-    min_quantity_of_videos = models.IntegerField(blank=True, null=True)
-    min_quantity_of_images = models.IntegerField(blank=True, null=True)
-    min_quantity_of_gifs = models.IntegerField(blank=True, null=True)
-    min_text_length = models.IntegerField(blank=True, null=True)
+    min_text_length = models.IntegerField(blank=True, null=True,
+                                          verbose_name='Минимальная длина текста')
+    min_quantity_of_line_breaks = models.IntegerField(blank=True, null=True,
+                                                      verbose_name='Минимальное количество переносов строк')
+    min_quantity_of_videos = models.IntegerField(blank=True, null=True,
+                                                 verbose_name='Минимальное количество видео')
+    min_quantity_of_images = models.IntegerField(blank=True, null=True,
+                                                 verbose_name='Минимальное количество изображений')
+    min_quantity_of_gifs = models.IntegerField(blank=True, null=True,
+                                               verbose_name='Минимальное количество гифок')
+
+    def __str__(self):
+        return 'Фильтр #{} для группы {}'.format(self.id, self.donor)
 
 
 class Record(models.Model):
