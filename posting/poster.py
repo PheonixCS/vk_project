109a0e433d240cc149ea9a9d99395a77719c5a82
Alexander Bogdanov <netolyrg@gmail.com>
2018-05-14 12:42:37 +0300

    added periodic and async tasks

--- posting/poster.py
+++ posting/poster.py
@@ -1,4 +1,4 @@
-import datetime
+#
 import os
 
 import vk_api
@@ -6,7 +6,6 @@ import requests
 import logging
 from PIL import Image
 
-from posting.models import Group
 from settings.models import Setting
 
 log = logging.getLogger('posting.poster')
@@ -69,69 +68,8 @@ def upload_photo(api, photo_url, group_id):
     return 'photo{}_{}'.format(photo[0]['owner_id'], photo[0]['id'])
 
 
-def post_record(api, group_id, record):
-    try:
-        attachments = list()
-
-        videos = record.videos.all()
-        if videos:
-            for video in videos:
-                attachments.append(upload_video(api, video.get_url(), group_id))
-
-        images = record.images.all()
-        if images:
-            for image in images:
-                attachments.append(upload_photo(api, image.url, group_id))
-
-        api.wall.post(owner_id='-{}'.format(group_id),
-                      from_group=1,
-                      message=record.text,
-                      attachments=','.join(attachments))
-    except vk_api.VkApiError as error_msg:
-        log.info('group {} got api error: {}'.format(group_id, error_msg))
-        return False
-
-    return True
-
-
 def fetch_group_id(api, domain_or_id):
     if domain_or_id.isdigit():
         return domain_or_id
     else:
         return api.utils.resolveScreenName(domain_or_id)['object_id']
-
-
-def main():
-    log.info('start main scrapper')
-    groups_to_post_in = Group.objects.filter(user__isnull=False, donors__isnull=False)
-
-    for group in groups_to_post_in:
-        log.debug('working with group {}'.format(group.id))
-
-        api = create_vk_api_using_login_password(group.user.login, group.user.password, group.user.app_id)
-        if not api:
-            continue
-
-        if not group.group_id:
-            group.group_id = fetch_group_id(api, group.domain_or_id)
-            group.save(update_fields=['group_id'])
-
-        if group.posting_time.minute == datetime.datetime.now().minute:
-            records = [record for donor in group.donors.all() for record in
-                       donor.records.filter(rate__isnull=False, post_in_group_date__isnull=True)]
-            log.debug('got {} ready to post records'.format(len(records)))
-            if not records:
-                continue
-
-            record_with_max_rate = max(records, key=lambda x:x.rate)
-            log.debug('record {} got max rate'.format(record_with_max_rate))
-
-            response = post_record(api, group.id, record_with_max_rate)
-
-            if response:
-                record_with_max_rate.post_in_group_date = datetime.datetime.now()
-                record_with_max_rate.save(update_fields=['post_in_group_date'])
-
-
-if __name__ == '__main__':
-    main()
