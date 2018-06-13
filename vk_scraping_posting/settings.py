f95731e9675b6f4d419dd2aaa6147acbf198c340
Alexander Bogdanov <netolyrg@gmail.com>
2018-06-13 05:14:16 +0300

    delete old ads task added

--- vk_scraping_posting/settings.py
+++ vk_scraping_posting/settings.py
@@ -34,6 +34,10 @@ CELERY_BEAT_SCHEDULE = {
     'delete_old_task': {
         'task': 'scraping.tasks.delete_oldest',
         'schedule': crontab(minute=0, hour=5)  # at 5:00 am UTC (8:00 am by MSK)
+    },
+    'delete_old_ads': {
+        'task': 'posting.tasks.delete_old_ads',
+        'schedule': crontab(minute=30)  # every hour at 30 minute
     }
 }
 
