96bc4d5a3cc0b6d9ead6f9bac2bf191bf9ee0211
Alexander Bogdanov <netolyrg@gmail.com>
2018-07-01 16:44:17 +0300

    fuck regex

--- posting/poster.py
+++ posting/poster.py
@@ -216,16 +216,9 @@ def delete_hashtags_from_text(text):
 
 def delete_emoji_from_text(text):
     log.debug('delete_emoji_from_text called. Text: "{}"'.format(text))
-    # emoji_pattern = re.compile(
-    #     u"(\ud83d[\ude00-\ude4f])|"  # emoticons
-    #     u"(\ud83c[\udf00-\uffff])|"  # symbols & pictographs (1 of 2)
-    #     u"(\ud83d[\u0000-\uddff])|"  # symbols & pictographs (2 of 2)
-    #     u"(\ud83d[\ude80-\udeff])|"  # transport & map symbols
-    #     u"(\ud83c[\udde0-\uddff])"  # flags (iOS)
-    #     "+", flags=re.UNICODE)
-    # text_without_emoji = re.sub(r'([0-9]?&#\d+;)', '', text)
-    # text_without_emoji = emoji_pattern.sub('', text)
-    text_without_emoji = re.sub(u'[\u0000-\u052F]+', ' ', text)
+    # text_without_emoji = re.sub(u'[\u0000-\u052F]+', ' ', text)
+    last_char_code = ord('я')
+    text_without_emoji = ''.join(letter for letter in text if ord(letter) <= last_char_code)
     log.debug('text after deleting "{}"'.format(text_without_emoji))
     text_without_double_spaces = delete_double_spaces_from_text(text_without_emoji)
     return text_without_double_spaces
