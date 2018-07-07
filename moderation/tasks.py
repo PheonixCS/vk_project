73667a03ea1fce905ae64f64031d62a24cca751e
Odnoral <alexander.odnoral@gmail.com>
2018-07-07 23:20:04 +0300

    fix

--- moderation/tasks.py
+++ moderation/tasks.py
@@ -15,22 +15,19 @@ def process_transactions():
     unprocessed_transactions = get_transactions_to_process()
     log.info('got {} unprocessed comment transactions'.format(len(unprocessed_transactions)))
 
-    try:
-        for transaction in unprocessed_transactions:
-            try:
-                process_comment(transaction.body)
-                save_comment_to_db(transaction)
+    for transaction in unprocessed_transactions:
+        try:
+            process_comment(transaction.body)
+            save_comment_to_db(transaction)
 
-                transaction.status = WebhookTransaction.PROCESSED
-                transaction.save(update_fields=['status'])
+            transaction.status = WebhookTransaction.PROCESSED
+            transaction.save(update_fields=['status'])
 
-            except:
-                transaction.status = WebhookTransaction.ERROR
-                transaction.save(update_fields=['status'])
+        except:
+            transaction.status = WebhookTransaction.ERROR
+            transaction.save(update_fields=['status'])
 
-                log.error('caught unexpected exception in process comment {}'.format(transaction.body['object']['id']),
-                          exc_info=True)
-    except:
-        log.error('', exc_info=True)
+            log.error('caught unexpected exception in process comment {}'.format(transaction.body['object']['id']),
+                      exc_info=True)
 
     log.info('process_transactions task completed')
