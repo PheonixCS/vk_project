659a64e8075b376174d85580037b98b38501cda7
Alexander Bogdanov <netolyrg@gmail.com>
2018-07-07 20:29:16 +0300

    logging

--- moderation/views.py
+++ moderation/views.py
@@ -2,6 +2,8 @@ import copy
 import datetime
 import json
 
+import logging
+
 from django.http import HttpResponse
 from django.utils import timezone
 from django.views.decorators.csrf import csrf_exempt
@@ -11,9 +13,12 @@ from moderation import core
 from moderation.models import WebhookTransaction
 
 
+log = logging.getLogger('moderation.views')
+
 @csrf_exempt
 @require_POST
 def webhook(request):
+    log.debug('opop')
     received_json_data = json.loads(request.body.decode("utf-8"))
 
     # meta = copy.copy(request.META)
@@ -21,15 +26,18 @@ def webhook(request):
     #     if not isinstance(v, str):
     #         del meta[k]
 
-    WebhookTransaction.objects.create(
-        date_generated=datetime.datetime.fromtimestamp(
-            received_json_data['timestamp'],
-            tz=timezone.utc
-        ),
-        body=received_json_data
-    )
+    try:
+        WebhookTransaction.objects.create(
+            date_generated=datetime.datetime.fromtimestamp(
+                received_json_data['timestamp'],
+                tz=timezone.utc
+            ),
+            body=received_json_data
+        )
 
-    if received_json_data['type'] == 'confirmation' and core.does_group_exist(received_json_data['group_id']):
-        return HttpResponse(core.get_callback_api_key(received_json_data['group_id']))
+        if received_json_data['type'] == 'confirmation' and core.does_group_exist(received_json_data['group_id']):
+            return HttpResponse(core.get_callback_api_key(received_json_data['group_id']))
+    except:
+        log.error('', exc_info=True)
 
     return HttpResponse(status=200)
