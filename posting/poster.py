8f4361f01d771e0339383d1d3d751fa50850b942
Odnoral <alexander.odnoral@gmail.com>
2018-07-16 14:15:16 +0300

    add logs

--- posting/poster.py
+++ posting/poster.py
@@ -223,23 +223,28 @@ def is_all_images_of_same_size(files):
     return image_sizes[1:] == image_sizes[:-1]
 
 
-def merge_six_images_into_one(files, filepath=os.path.join(settings.BASE_DIR, 'merged_image.jpg')):
+def merge_six_images_into_one(files):
+    log.debug('merge_six_images_into_one called')
     img = Image.open(os.path.join(settings.BASE_DIR, files[0]))
-    width, height = img.size()
+    width, height = img.size
 
     result = Image.new("RGB", (width * 3, height * 2))
 
     for index, file in enumerate(files):
+        log.debug('start work with image {}'.format(file))
         img = Image.open(os.path.join(settings.BASE_DIR, file))
-        img.thumbnail((width, height), Image.ANTIALIAS)
+
         x = index // 2 * width
         y = index % 2 * height
         result.paste(img, (x, y, x + width, y + height))
+
+        log.debug('image {} deleted'.format(file))
         os.remove(file)
 
-    result.save(filepath)
+    result.save(files[0])
 
-    return filepath
+    log.debug('merge_six_images_into_one finished')
+    return files[0]
 
 
 def prepare_image_for_posting(image_local_filepath, **kwargs):
