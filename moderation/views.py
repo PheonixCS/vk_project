47cc57d66be8c7ff8f5b3a283d71d941ffbb67e4
Alexander Bogdanov <netolyrg@gmail.com>
2018-08-08 02:53:03 +0300

    [92] delete settings app

--- moderation/views.py
+++ moderation/views.py
@@ -1,11 +1,8 @@
-import copy
-import datetime
 import json
 
 import logging
 
 from django.http import HttpResponse
-from django.utils import timezone
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 
--- vk_scraping_posting/config.py
+++
@@ -1,9 +0,0 @@
-settings = {
-    'VK_API_VERSION': 5.74,
-    'MIN_STRING_MATCH_RATIO': 0.85,
-    'PIXELS_TO_CUT_FROM_BOTTOM': 10
-}
-
-callback_api_settings = {
-    #group_id: confirmation_string
-}
