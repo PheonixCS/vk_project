97d3882d384503237871961047597b0680bcb4f4
Alexander Bogdanov <netolyrg@gmail.com>
2018-08-25 21:51:23 +0300

    add letters map

---
+++ posting/text_utilities.py
@@ -0,0 +1,26 @@
+#
+
+
+def replace_russian_with_english_letters(text):
+
+    # replace map goes with russian letter as a key
+    replace_map = {
+        'а': 'а',
+        'с': 'c',
+        'o': 'o',
+        'е': 'e',
+        'у': 'y',
+        'х': 'x',
+        'р': 'p',
+        'Е': 'E',
+        'Н': 'H',
+        'О': 'O',
+        'Х': 'X',
+        'А': 'A',
+        'С': 'C',
+        'В': 'B',
+        'Р': 'P',
+        'M': 'M'
+    }
+
+
