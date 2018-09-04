ae3152d11f2a036d0f51c02270a80002363b1b62
Alexander Bogdanov <netolyrg@gmail.com>
2018-09-05 02:03:20 +0300

    implementation and test replacing russian letters

--- posting/text_utilities.py
+++ posting/text_utilities.py
@@ -1,13 +1,14 @@
 #
+import re
 
 
 def replace_russian_with_english_letters(text):
 
     # replace map goes with russian letter as a key
     replace_map = {
-        'а': 'а',
+        'а': 'a',
         'с': 'c',
-        'o': 'o',
+        'о': 'o',
         'е': 'e',
         'у': 'y',
         'х': 'x',
@@ -20,7 +21,20 @@ def replace_russian_with_english_letters(text):
         'С': 'C',
         'В': 'B',
         'Р': 'P',
-        'M': 'M'
+        'М': 'M'
     }
+    result_text = ''
+    replaced = False
 
+    for word in re.findall(r'\s*\S+\s*', text):
+        if not replaced:
+            for letter in word:
+                if letter in replace_map.keys():
+                    word = word.replace(letter, replace_map[letter], 1)
+                    replaced = True
+                    break
+        else:
+            replaced = False
+        result_text += word
 
+    return result_text
