860a7dbd460f1d5bdc336ebaa3b40a548c535a81
Alexander Bogdanov <netolyrg@gmail.com>
2018-09-29 15:40:00 +0300

    new letter replacement logic

--- posting/text_utilities.py
+++ posting/text_utilities.py
@@ -29,18 +29,24 @@ def replace_russian_with_english_letters(text):
         'к': 'k',
         'Ы': 'bI'
     }
-    result_text = ''
-    replaced = False
+    # Comment because of
+    # result_text = ''
+    # replaced = False
+    #
+    # for word in re.findall(r'\s*\S+\s*', text):
+    #     if not replaced:
+    #         for letter in word:
+    #             if letter in replace_map.keys():
+    #                 word = word.replace(letter, replace_map[letter], 1)
+    #                 replaced = True
+    #                 break
+    #     else:
+    #         replaced = False
+    #     result_text += word
 
-    for word in re.findall(r'\s*\S+\s*', text):
-        if not replaced:
-            for letter in word:
-                if letter in replace_map.keys():
-                    word = word.replace(letter, replace_map[letter], 1)
-                    replaced = True
-                    break
-        else:
-            replaced = False
-        result_text += word
+    result_text = text
+
+    for letter, replacement in replace_map.items():
+        result_text = re.sub(letter, replacement, result_text)
 
     return result_text
