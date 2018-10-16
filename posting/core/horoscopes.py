376c11f8cf006907dd37cfa5f5c82c3df77c885b
Odnoral <alexander.odnoral@gmail.com>
2018-10-16 16:20:44 +0300

    horoscopes hotfix

--- posting/core/horoscopes.py
+++ posting/core/horoscopes.py
@@ -1,3 +1,4 @@
+from posting.models import Group
 from scraping.core.horoscopes import fetch_zodiac_sign
 
 
@@ -5,7 +6,7 @@ def fetch_date_from_horoscope_text(raw_text):
     return raw_text.split('\n')[0].split(', ')[0]
 
 
-def generate_special_group_reference(horoscope_text, group_id):
+def generate_special_group_reference(horoscope_text):
     replace_map = {
         'Овен': 'Овна',
         'Телец': 'Тельца',
@@ -21,6 +22,9 @@ def generate_special_group_reference(horoscope_text, group_id):
         'Рыбы': 'Рыбы',
     }
 
+    special_group_zodiac_zign = fetch_zodiac_sign(horoscope_text).capitalize()
+    special_group_id = Group.objects.filter(name=special_group_zodiac_zign).first().domain_or_id
     horoscope_date = fetch_date_from_horoscope_text(horoscope_text)
-    return f'[club{group_id}|Гороскоп для {replace_map[fetch_zodiac_sign(horoscope_text).capitalize()]}] ' \
+    return f'[club{special_group_id}|' \
+           f'Гороскоп для {replace_map[special_group_zodiac_zign]}] ' \
            f'на {horoscope_date}'
--- posting/tasks.py
+++ posting/tasks.py
@@ -230,7 +230,7 @@ def post_horoscope(login, password, app_id, group_id, horoscope_record_id):
 
         group_zodiac_zign = fetch_zodiac_sign(group.name)
         if not group_zodiac_zign:
-            text_to_add = generate_special_group_reference(horoscope_record.text, group.domain_or_id)
+            text_to_add = generate_special_group_reference(horoscope_record.text)
             record_text = '\n'.join([text_to_add, record_text]) if record_text else text_to_add
 
         post_response = api.wall.post(owner_id='-{}'.format(group_id),
