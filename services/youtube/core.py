87d072a002ad3f6e87cc0687a35e93d9a2bf2cfb
Alexander Bogdanov <netolyrg@gmail.com>
2018-10-18 01:53:28 +0300

    [96] catch youtube downloading exception

--- services/youtube/core.py
+++ services/youtube/core.py
@@ -8,13 +8,19 @@ log = logging.getLogger('services.youtube')
 
 def download_trailer(url):
     log.debug('starting downloading trailer')
+    # TODO make downloading try except smarter
     try:
         path = pytube.YouTube(url).streams.first().download()
     except url_error.URLError:
         # this exception may occur, see https://github.com/nficano/pytube/issues/278
         import ssl
         ssl._create_default_https_context = ssl._create_stdlib_context
-        path = pytube.YouTube(url).streams.first().download()
+
+        try:
+            path = pytube.YouTube(url).streams.first().download()
+        except:
+            return ''
+
     # TODO check how catch VideoUnavailable
     # except pytube.__main__.VideoUnavailable:
     except:
