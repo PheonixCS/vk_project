bfb60e79a3964b38dc9fb886055ae321c21c7a67
Alexander Bogdanov <netolyrg@gmail.com>
2018-11-13 01:02:57 +0300

    files refactor

--- services/vk/files.py
+++ services/vk/files.py
@@ -1,5 +1,4 @@
 import logging
-import os
 
 import vk_api
 
@@ -8,57 +7,71 @@ from posting.poster import download_file
 log = logging.getLogger('services.vk.files')
 
 
-def upload_video(session, video_local_filename, group_id, name, description):
+def upload_video(session, video_local_path, group_id, name, description):
     log.debug('upload_video called')
 
     try:
         upload = vk_api.VkUpload(session)
-        video = upload.video(video_file=video_local_filename,
-                             group_id=int(group_id),
-                             name=name,
-                             description=description,
-                             no_comments=True)
-    except:
-        log.error('exception while uploading video', exc_info=True)
+        video = upload.video(
+            video_file=video_local_path,
+            group_id=int(group_id),
+            name=name,
+            description=description,
+            no_comments=True
+        )
+    except vk_api.VkApiError:
+        log.error('vk exception while uploading video', exc_info=True)
         return
 
-    return 'video{}_{}'.format(video['owner_id'], video['video_id'])
+    owner_id, video_id = video.get('owner_id'), video.get('video_id')
+
+    if owner_id and video_id:
+        return 'video{}_{}'.format(owner_id, video_id)
 
 
 def upload_gif(session, gif_url):
     log.debug('upload_gif called')
+
     gif_local_filename = download_file(gif_url, 'gif')
 
     try:
         upload = vk_api.VkUpload(session)
         gif = upload.document(doc=gif_local_filename)
-    except:
-        log.error('exception while uploading gif', exc_info=True)
+    except vk_api.VkApiError:
+        log.error('vk exception while uploading gif', exc_info=True)
         return
 
-    if os.path.isfile(gif_local_filename):
-        os.remove(gif_local_filename)
+    if gif and isinstance(gif, list):
+        owner_id, gif_id = gif[0].get('owner_id'), gif[0].get('id')
 
-    return 'doc{}_{}'.format(gif[0]['owner_id'], gif[0]['id'])
+        if owner_id and gif_id:
+            return 'doc{}_{}'.format(owner_id, gif_id)
 
 
-def upload_photo(session, image_local_filepath, group_id):
+def upload_photo(session, image_local_path, group_id):
     log.debug('upload_photo called')
 
     try:
         upload = vk_api.VkUpload(session)
-        photo = upload.photo_wall(photos=image_local_filepath,
-                                  group_id=int(group_id))
-    except:
-        log.error('exception while uploading photo', exc_info=True)
+        photo = upload.photo_wall(
+            photos=image_local_path,
+            group_id=int(group_id)
+        )
+    except vk_api.VkApiError:
+        log.error('vk exception while uploading photo', exc_info=True)
         return
 
-    return 'photo{}_{}'.format(photo[0]['owner_id'], photo[0]['id'])
+    if photo and isinstance(photo, list):
+        owner_id, photo_id = photo[0].get('owner_id'), photo[0].get('id')
+
+        if owner_id and photo_id:
+            return 'photo{}_{}'.format(owner_id, photo_id)
 
 
 def check_docs_availability(api, docs):
     """
 
+    :param api: api object
     :param docs: list of dictionaries
     :type docs: list
     :return: true if all docs are available
@@ -69,12 +82,11 @@ def check_docs_availability(api, docs):
     try:
         resp = api.docs.getById(docs=','.join(docs))
 
-        if len(resp) == len(docs):
+        if isinstance(resp, list) and len(resp) == len(docs):
             return True
         else:
             log.info('check_docs_availability failed')
             return False
-
     except:
         log.error('got unexpected error in check_docs_availability', exc_info=True)
 
@@ -87,17 +99,15 @@ def check_video_availability(api, owner_id, video_id):
     :param owner_id: string  representing video owner in vk way
     :return: true if video available
     """
-
     log.debug('check_video_availability called')
 
     try:
-        resp = api.video.get(owner_id=owner_id, videos='{}_{}'.format(owner_id, video_id))
+        resp = api.video.get(owner_id=owner_id, videos=f'{owner_id}_{video_id}')
 
         if resp.get('items'):
             return True
         else:
             log.info('check_video_availability failed')
             return False
-
     except:
         log.error('got unexpected error in check_video_availability', exc_info=True)
