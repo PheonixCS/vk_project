483a6cfb1275f262ff1924d6841ab6e7d5194777
Alexander Bogdanov <netolyrg@gmail.com>
2018-12-13 13:23:34 +0300

    new api request, command half done

--- scraping/management/commands/fix_vk_trailers.py
+++ scraping/management/commands/fix_vk_trailers.py
@@ -1,6 +1,8 @@
 # This command solve 152 task problem: parse
 
 from django.core.management.base import BaseCommand
+from services.vk import core, videos
+from posting.models import Group
 
 
 class Command(BaseCommand):
@@ -10,9 +12,20 @@ class Command(BaseCommand):
         parser.add_argument(
             '-d',
             '--group_id',
-            default='27045802',
+            type=int,
+            default=27045802,
             help='id of the group'
         )
 
     def handle(self, *args, **options):
-        pass
+        group_id = options['group_id']
+
+        group = Group.objects.get(group_id=group_id)
+
+        api = core.create_vk_session_using_login_password(
+            group.user.login,
+            group.user.password,
+            group.user.app_id
+        ).get_api()
+
+        results = videos.get_all_group_videos(api, group_id)
