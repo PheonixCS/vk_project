e6f53af85a7e01df51039bdbf5941058b2c72d6b
Александр Богданов <netolyrg@gmail.com>
2019-02-23 23:04:35 +0300

    fix tasks

--- posting/tasks.py
+++ posting/tasks.py
@@ -235,7 +235,7 @@ def examine_groups():
                 the_best_record.save(update_fields=['is_involved_now'])
 
 
-@task
+@shared_task
 def post_music(login, password, app_id, group_id, record_id):
     session = create_vk_session_using_login_password(login, password, app_id)
     api = session.get_api()
@@ -348,88 +348,72 @@ def post_movie(group_id, movie_id):
         log.error('Number of images is not equal 3', exc_info=True)
 
     try:
+
         if config.ENABLE_MERGE_IMAGES_MOVIES:
             poster_file = download_file(movie.poster)
             poster_and_three_images = merge_poster_and_three_images(poster_file, image_files)
+            delete_files(image_files)
+            delete_files(poster_file)
 
             attachments.append(upload_photo(
                 session,
                 poster_and_three_images,
                 group_id)
             )
-
-            delete_files(poster_file)
             delete_files(poster_and_three_images)
         else:
             attachments.append(upload_photo(session, download_file(movie.poster), group_id))
             for image in image_files[:2]:
                 attachments.append(upload_photo(session, image, group_id))
+            delete_files(image_files)
 
-    if config.ENABLE_MERGE_IMAGES_MOVIES:
-        poster_file = download_file(movie.poster)
-        poster_and_three_images = merge_poster_and_three_images(poster_file, image_files)
-        delete_files(image_files)
-        delete_files(poster_file)
+            log.debug(f'movie {movie.title} post: got attachments {attachments}')
 
-        attachments.append(upload_photo(
-            session,
-            poster_and_three_images,
-            group_id)
-        )
-        delete_files(poster_and_three_images)
-    else:
-        attachments.append(upload_photo(session, download_file(movie.poster), group_id))
-        for image in image_files[:2]:
-            attachments.append(upload_photo(session, image, group_id))
-        delete_files(image_files)
+            uploaded_trailers = movie.trailers.filter(vk_url__isnull=False)
+            downloaded_trailers = movie.trailers.filter(status=Trailer.DOWNLOADED_STATUS)
 
-        log.debug(f'movie {movie.title} post: got attachments {attachments}')
+            if uploaded_trailers.exists():
+                trailer = uploaded_trailers.first()
+                uploaded_trailer = trailer.vk_url
+            elif downloaded_trailers.exists():
+                trailer = downloaded_trailers.first()
+                uploaded_trailer = upload_video(session, trailer.file_path, group_id, trailer_name, video_description)
 
-        uploaded_trailers = movie.trailers.filter(vk_url__isnull=False)
-        downloaded_trailers = movie.trailers.filter(status=Trailer.DOWNLOADED_STATUS)
-
-        if uploaded_trailers.exists():
-            trailer = uploaded_trailers.first()
-            uploaded_trailer = trailer.vk_url
-        elif downloaded_trailers.exists():
-            trailer = downloaded_trailers.first()
-            uploaded_trailer = upload_video(session, trailer.file_path, group_id, trailer_name, video_description)
-
-            if uploaded_trailer:
-                delete_files(trailer.file_path)
-                trailer.vk_url = uploaded_trailer
-                trailer.status = Trailer.UPLOADED_STATUS
+                if uploaded_trailer:
+                    delete_files(trailer.file_path)
+                    trailer.vk_url = uploaded_trailer
+                    trailer.status = Trailer.UPLOADED_STATUS
+                else:
+                    log.warning('failed to upload trailer')
             else:
-                log.warning('failed to upload trailer')
-        else:
-            log.error(f'movie {movie.title} got no trailer!')
-            uploaded_trailer = None
-            trailer = None
+                log.error(f'movie {movie.title} got no trailer!')
+                uploaded_trailer = None
+                trailer = None
 
-        if config.PUT_TRAILERS_TO_ATTACHMENTS and uploaded_trailer:
-            attachments.append(uploaded_trailer)
-            trailer_link = ''
-        else:
-            trailer_link = f'Трейлер: vk.com/{uploaded_trailer}'
+            if config.PUT_TRAILERS_TO_ATTACHMENTS and uploaded_trailer:
+                attachments.append(uploaded_trailer)
+                trailer_link = ''
+            else:
+                trailer_link = f'Трейлер: vk.com/{uploaded_trailer}'
 
-        if uploaded_trailer and trailer:
-            trailer.save(update_fields=['status', 'vk_url'])
+            if uploaded_trailer and trailer:
+                trailer.save(update_fields=['status', 'vk_url'])
 
-        record_text = f'{trailer_name}\n\n' \
-                      f'{trailer_information}\n\n' \
-                      f'{trailer_link if uploaded_trailer else ""}\n\n' \
-                      f'{movie.overview}'
+            record_text = f'{trailer_name}\n\n' \
+                          f'{trailer_information}\n\n' \
+                          f'{trailer_link if uploaded_trailer else ""}\n\n' \
+                          f'{movie.overview}'
 
-        post_response = api.wall.post(owner_id=f'-{group_id}',
-                                      from_group=1,
-                                      message=record_text,
-                                      attachments=','.join(attachments))
+            post_response = api.wall.post(owner_id=f'-{group_id}',
+                                          from_group=1,
+                                          message=record_text,
+                                          attachments=','.join(attachments))
 
-        movie.post_in_group_date = datetime.now(tz=timezone.utc).strftime('%Y-%m-%d %H:%M:%S')
-        movie.group = Group.objects.get(group_id=group_id)
-        movie.save(update_fields=['post_in_group_date', 'group'])
+            movie.post_in_group_date = datetime.now(tz=timezone.utc).strftime('%Y-%m-%d %H:%M:%S')
+            movie.group = Group.objects.get(group_id=group_id)
+            movie.save(update_fields=['post_in_group_date', 'group'])
 
-        log.debug(f'{post_response} in group {group_id}')
+            log.debug(f'{post_response} in group {group_id}')
     except:
         log.error('error in movie posting', exc_info=True)
 
