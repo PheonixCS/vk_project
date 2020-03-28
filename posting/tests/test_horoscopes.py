987bd25398e5e449719405b5973c2d5d9579d43b
Александр Богданов <netolyrg@gmail.com>
2020-03-29 02:42:26 +0300

    refactor tests

--- posting/tests/test_horoscopes.py
+++ posting/tests/test_horoscopes.py
@@ -1,13 +1,12 @@
-from django.test import TestCase
-
 from posting.core.horoscopes_images import transfer_horoscope_to_image
+import os
+
+example_text = 'Овны\nБлагоприятный день. Вы настроены решительно, готовы преодолеть любые преграды, которые ' \
+               'возникают на пути. Чаще обычного приходится хитрить и изворачиваться, это немного смущает, но вы '
 
 
-class HoroscopeImagesTests(TestCase):
-    example_text = 'Овны\nБлагоприятный день. Вы настроены решительно, готовы преодолеть любые преграды, которые ' \
-                   'возникают на пути. Чаще обычного приходится хитрить и изворачиваться, это немного смущает, но вы '
+def test_horoscope_to_image():
+    file_name = transfer_horoscope_to_image(example_text)
 
-    def test_common(self):
-        file_name = transfer_horoscope_to_image(self.example_text)
-
-        self.assertIsNotNone(file_name)
+    assert file_name is not None
+    os.remove(file_name)
--- posting/tests/test_images.py
+++ posting/tests/test_images.py
@@ -1,49 +1,35 @@
-#
-
 from PIL import Image
-from django.test import TestCase
 
 import posting.core.images as images
 
 
 # Create your tests here.
-class ImagesTests(TestCase):
-
+class TestImages:
     def test_size_calculation_width(self):
         origin_size = (1920, 1080)
 
         new_size = images.calculate_size_from_one_side(*origin_size, width=1280)
 
-        self.assertEqual(new_size, (1280, 720))
+        assert new_size == (1280, 720)
 
     def test_size_calculation_height(self):
         origin_size = (1920, 1080)
 
         new_size = images.calculate_size_from_one_side(*origin_size, height=720)
 
-        self.assertEqual(new_size, (1280, 720))
+        assert new_size == (1280, 720)
 
-    def test_text_on_images(self):
-        image_name = 'test.jpg'
+    def test_text_on_images(self, tmp_path):
+        image_name = str(tmp_path / 'test.jpg')
 
         img = Image.new('RGB', (500, 500), color='white')
-        img.save(image_name)
+        img.save(image_name, 'jpeg')
 
         text = '20 of seasdfasdfasdfasdfasdfasdfasdfasdfasdfaptember, Text\nLol kek\nCheburek'
 
-        images.paste_text_on_image(image_name, text, position='top')
+        result = images.paste_text_on_image(image_name, text, position='top')
 
-        self.assertTrue(True)
-
-    def test_old_text_on_images(self):
-        image_name = 'test_old.jpg'
-
-        img = Image.new('RGB', (500, 500), color='white')
-        img.save(image_name)
-
-        text = '20 of seasdfasdfasdfasdfasdfasdfasdfasdfasdfaptember, Text\nLol kek\nCheburek'
-
-        images.fill_image_with_text(image_name, text)
+        assert result
 
     def test_position_calculation(self):
         image_box = (100, 100)
@@ -51,7 +37,7 @@ class ImagesTests(TestCase):
 
         position = images.calculate_text_position_on_image(image_box, text_box, 'top')
 
-        self.assertEqual(position, (40, 10))
+        assert position == (40, 10)
 
     def test_position_calculation_bottom(self):
         image_box = (100, 100)
@@ -59,7 +45,7 @@ class ImagesTests(TestCase):
 
         position = images.calculate_text_position_on_image(image_box, text_box, 'bottom')
 
-        self.assertEqual(position, (40, 85))
+        assert position == (40, 85)
 
     # def test_abstraction(self):
     #     template_image = os.path.join(settings.BASE_DIR, 'posting/extras/image_templates', 'disc_template.png')
@@ -73,7 +59,7 @@ class ImagesTests(TestCase):
 
         result_box = images.calculate_centered_box(size_two, size)
 
-        self.assertEqual(result_box, expected_box)
+        assert result_box == expected_box
 
     def test_calculate_box_equal_size(self):
         size = (800, 600)
@@ -82,14 +68,14 @@ class ImagesTests(TestCase):
 
         result_box = images.calculate_centered_box(size_two, size)
 
-        self.assertEqual(result_box, expected_box)
+        assert result_box == expected_box
 
     def test_center_box(self):
         img = Image.new('RGB', (500, 500), color='white')
 
         result = images.crop_center(img, (300, 300))
 
-        self.assertEqual(result.size, (300, 300))
+        assert result.size == (300, 300)
 
     def test_position_calculation_with_offset_bottom(self):
         image_size = (100, 100)
@@ -97,7 +83,7 @@ class ImagesTests(TestCase):
 
         position = images.calculate_text_position_on_image(image_size, text_size, 'bottom', offset=10)
 
-        self.assertEqual(position, (40, 75))
+        assert position == (40, 75)
 
     def test_position_calculation_with_offset_top(self):
         image_size = (100, 100)
@@ -105,4 +91,4 @@ class ImagesTests(TestCase):
 
         position = images.calculate_text_position_on_image(image_size, text_size, 'top', offset=10)
 
-        self.assertEqual(position, (40, 20))
+        assert position == (40, 20)
