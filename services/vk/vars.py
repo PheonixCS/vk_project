918d193e4fa765ed2d8ba6dc7126f0ba2f0a57b5
Александр Богданов <netolyrg@gmail.com>
2020-09-01 21:03:12 +0300

    get wall handle

--- services/vk/vars.py
+++ services/vk/vars.py
@@ -6,4 +6,5 @@ GROUP_IS_BANNED = 'group'
 BANNED_GROUP_ERROR_MESSAGE = 'Access denied: group is blocked'
 BANNED_ACCOUNT_ERROR_MESSAGE = 'Account is blocked'
 ACCESS_TOKEN_AUTH_FAIL = 'User authorization failed: no access_token passed.'
-ADVERTISEMENT_ERROR_CODE = '219'
+ADVERTISEMENT_ERROR_CODE = 219
+RATE_LIMIT_CODE = 29
