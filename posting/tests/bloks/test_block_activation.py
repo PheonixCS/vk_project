2bcaaa88e09b2eb6ce56ba0d916b884d8d427ffa
Александр Богданов <netolyrg@gmail.com>
2020-09-26 10:24:50 +0300

    fix interval and add tets

--- posting/tests/bloks/test_block_activation.py
+++ posting/tests/bloks/test_block_activation.py
@@ -1,4 +1,5 @@
 from posting.models import Block
+from django.utils import timezone
 
 
 def test_activation(create_group, freezer):
@@ -35,3 +36,16 @@ def test_activation_border(create_group, freezer):
     result = group.is_blocked()
 
     assert result is True
+
+
+def test_interval(create_group, freezer):
+    group = create_group()
+    freezer.move_to('2020-09-09 12:00:00')
+    interval = (group.get_next_posting_time() - timezone.now()).seconds // 60
+    group.set_block(Block.RECENT_POSTS, period_in_minutes=interval)
+
+    freezer.move_to('2020-09-09 13:00:00')
+    result = group.is_blocked()
+
+    assert result is False
+
