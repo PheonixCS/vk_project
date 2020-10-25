097ceba437039fbe3deb472e9ef728f1a6e55b94
Александр Богданов <netolyrg@gmail.com>
2020-10-25 22:55:19 +0300

    change time forr horoscopes only

--- posting/tasks/examine_groups.py
+++ posting/tasks/examine_groups.py
@@ -220,9 +220,8 @@ def find_common_record_to_post(group: Group) -> Tuple[Record or None, List[Recor
     today_start = now_time_utc.replace(hour=0, minute=0, second=0)
     allowed_time_threshold = now_time_utc - timedelta(hours=8)
 
-    # Remove it, because Pavel angry about this feature
-    # if allowed_time_threshold > today_start:
-    #     allowed_time_threshold = today_start
+    if group.group_type in (Group.HOROSCOPES_MAIN, Group.HOROSCOPES_COMMON) and allowed_time_threshold > today_start:
+        allowed_time_threshold = today_start
 
     donors = group.donors.all()
     if not donors:
