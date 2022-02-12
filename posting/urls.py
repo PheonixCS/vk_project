073ffeacf9e87a924c74a26eadc0c898b8069d38
abogdanov <netolyrg@gmail.com>
2022-02-12 19:36:18 +0300

    update template

--- posting/urls.py
+++ posting/urls.py
@@ -6,7 +6,7 @@ app_name = 'posting'
 urlpatterns = [
     path('', IndexView.as_view(), name='posting'),
     path('group/<int:pk>/', GroupView.as_view(), name='groups'),
-    path('users/', UsersView.as_view()),
+    path('users/', UsersView.as_view(), name='users'),
     path('users/<int:pk>', UserView.as_view(), name='user'),
     path('users/activate/<int:pk>', ActivateView.as_view(), name='activate')
 ]
