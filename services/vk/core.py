ae3b2ea563c95463339ca35335078530ae7ecc76
abogdanov <netolyrg@gmail.com>
2022-02-15 12:03:32 +0300

    add token only

--- services/vk/core.py
+++ services/vk/core.py
@@ -58,7 +58,7 @@ def create_vk_session_using_login_password(login, password, app_id, special_sess
             'User-agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36'
 
         log.debug('auth start')
-        vk_session.auth()
+        vk_session.auth(token_only=True)
         log.debug('auth end')
 
     except vk_api.TwoFactorError as err:
