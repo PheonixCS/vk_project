7a258b5397c8568bc01af574e610f4cbe98ca4d6
abogdanov <netolyrg@gmail.com>
2022-11-02 00:09:42 +0300

    add name

--- shapranov/urls.py
+++ shapranov/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from shapranov.views import DonateView, MainPageView, DocsView
 
 urlpatterns = [
-    path('', MainPageView.as_view()),
+    path('', MainPageView.as_view(), name='shapranov'),
     path('smysl-zhizni-cheloveka/', DonateView.as_view()),
     path('docs', DocsView.as_view(), name='docs'),
 ]
