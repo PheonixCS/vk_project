20228bb8ea943430f0858250e2129f3aa8d550d9
abogdanov <netolyrg@gmail.com>
2022-11-02 00:08:06 +0300

    add link in django admin

--- shapranov/urls.py
+++ shapranov/urls.py
@@ -5,5 +5,5 @@ from shapranov.views import DonateView, MainPageView, DocsView
 urlpatterns = [
     path('', MainPageView.as_view()),
     path('smysl-zhizni-cheloveka/', DonateView.as_view()),
-    path('docs', DocsView.as_view()),
+    path('docs', DocsView.as_view(), name='docs'),
 ]
