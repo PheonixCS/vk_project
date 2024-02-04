0a9194f736ad618adcfb789e8edee5ffd6dcc5a9
netolyrg <netolyrg@gmail.com>
2024-02-04 04:55:43 +0300

    SPB-8: support many sources

--- tg_core/management/commands/collect_ih_sources.py
+++ tg_core/management/commands/collect_ih_sources.py
@@ -29,67 +29,68 @@ class Command(BaseCommand):
         for channel in Channel.objects.filter(is_active=True).iterator():
             print(f'work with {channel}')
 
-            internal_horoscope_source = InternalHoroscopeSource.objects.filter(
+            internal_horoscope_sources = InternalHoroscopeSource.objects.filter(
                 channel=channel
-            ).first()
+            )
 
             last_day = timezone.now() - timedelta(days=1)
 
-            link_objects = InternalHoroscopeSourceLink.objects.filter(
-                link=internal_horoscope_source,
-                created_dt__gte=last_day,
-            )
-
-            if link_objects.exists():
-                last_linked = link_objects.values_list('source_post', flat=True)
-
-                last_not_linked_horoscopes = Horoscope.objects.filter(
-                    post_in_group_date__gte=last_day
-                ).exclude(
-                    pk__in=last_linked,  # ERROR must be last linked by source_post
-                )
-            else:
-                last_not_linked_horoscopes = Horoscope.objects.filter(
-                    post_in_group_date__gte=last_day
-                )
-
-            for horoscope in last_not_linked_horoscopes.iterator():
-                log.debug(f'Adding {horoscope}')
-                print(f'Adding {horoscope}')
-
-                tg_post = TGPost.objects.create(
-                    text=Horoscope.text,
-                    channel=channel,
-                )
-
-                InternalHoroscopeSourceLink.objects.create(
+            for internal_horoscope_source in internal_horoscope_sources:
+                link_objects = InternalHoroscopeSourceLink.objects.filter(
                     link=internal_horoscope_source,
-                    target_post=tg_post,
-                    source_post=horoscope
+                    created_dt__gte=last_day,
                 )
 
-                image_object = transfer_horoscope_to_image_object(horoscope.text)
-                image_object = paste_horoscopes_rates_object(image_object)
+                if link_objects.exists():
+                    last_linked = link_objects.values_list('source_post', flat=True)
 
-                buffer = BytesIO()
-                image_object.save(buffer, format='JPEG')
-                image_object = ContentFile(buffer.getvalue())
+                    last_not_linked_horoscopes = Horoscope.objects.filter(
+                        post_in_group_date__gte=last_day
+                    ).exclude(
+                        pk__in=last_linked,  # ERROR must be last linked by source_post
+                    )
+                else:
+                    last_not_linked_horoscopes = Horoscope.objects.filter(
+                        post_in_group_date__gte=last_day
+                    )
 
-                print('ready to paste django file')
+                for horoscope in last_not_linked_horoscopes.iterator():
+                    log.debug(f'Adding {horoscope}')
+                    print(f'Adding {horoscope}')
 
-                django_file = InMemoryUploadedFile(
-                    image_object,
-                    None,
-                    f'{str(uuid.uuid4())}.jpg',
-                    'image/jpeg',
-                    image_object.tell,
-                    None
-                )
+                    tg_post = TGPost.objects.create(
+                        text=Horoscope.text,
+                        channel=channel,
+                    )
 
-                TGAttachment.objects.create(
-                    file=django_file,
-                    post=tg_post,
-                )
+                    InternalHoroscopeSourceLink.objects.create(
+                        link=internal_horoscope_source,
+                        target_post=tg_post,
+                        source_post=horoscope
+                    )
 
-                log.debug(f'Done {horoscope}')
-                print(f'Done {horoscope}')
+                    image_object = transfer_horoscope_to_image_object(horoscope.text)
+                    image_object = paste_horoscopes_rates_object(image_object)
+
+                    buffer = BytesIO()
+                    image_object.save(buffer, format='JPEG')
+                    image_object = ContentFile(buffer.getvalue())
+
+                    print('ready to paste django file')
+
+                    django_file = InMemoryUploadedFile(
+                        image_object,
+                        None,
+                        f'{str(uuid.uuid4())}.jpg',
+                        'image/jpeg',
+                        image_object.tell,
+                        None
+                    )
+
+                    TGAttachment.objects.create(
+                        file=django_file,
+                        post=tg_post,
+                    )
+
+                    log.debug(f'Done {horoscope}')
+                    print(f'Done {horoscope}')
