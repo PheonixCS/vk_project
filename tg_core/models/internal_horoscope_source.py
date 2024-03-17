5230fd3ad155017db684414d88f6e4c3caedbfd0
netolyrg <netolyrg@gmail.com>
2024-03-17 18:26:57 +0300

    SPB-37: add str

--- tg_core/models/internal_horoscope_source.py
+++ tg_core/models/internal_horoscope_source.py
@@ -8,3 +8,9 @@ from tg_core.models.base import BaseModel
 class InternalHoroscopeSource(BaseModel):
     group = models.ForeignKey('posting.Group', on_delete=models.SET_NULL, null=True)
     repost_time = models.TimeField(default=datetime.time(00, 00), verbose_name='Время постинга из источника')
+
+    def __str__(self):
+        return self.__repr__()
+
+    def __repr__(self):
+        return f'InternalHoroscopeSource {self.group}, {self.repost_time}'
